efded8d71fe608ca79c4292878bc41b82c6a2691
ricky80022 <105021088@live.asia.edu.tw>
2016-09-21 12:19:28 +0800

    work1
    123

---
+++ src/work1/work1.java
@@ -0,0 +1,14 @@
+package work1;
+
+public class work1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        String nickName = "資工人";
+        String uni = "亞州大學";
+        System.out.println(nickName);
+        System.out.print(uni+"\n");
+        System.out.println(nickName+"   "+ uni);
+	}
+
+}
